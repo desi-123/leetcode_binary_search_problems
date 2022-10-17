a56ffd57510cedd48be337e68ec6d8ad455db4e1
desi-123 <desiemeng21m@gmail.com>
2022-10-16 21:37:40 -0700

    part one

---
+++ connect.py
@@ -0,0 +1,13 @@
+def connect(node):
+    cur, nxt = node, node.left if node else None
+
+    while cur and nxt:
+        cur.left.next = cur.right
+        if cur.next:
+            cur.right.next = cur.next.left
+
+        cur = cur.next
+        if not cur:
+            cur = nxt
+            nxt = cur.left
+    return node
